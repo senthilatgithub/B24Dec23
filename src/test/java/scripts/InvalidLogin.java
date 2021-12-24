5547ba57b67512729a52ca2595a94051f2da2c4c
senthilkumar <senthilkumar@AP-PC1QD26T>
2021-12-24 08:45:51 +0530

    values from Excel

--- src/test/java/scripts/InvalidLogin.java
+++ src/test/java/scripts/InvalidLogin.java
@@ -3,22 +3,29 @@ package scripts;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Excel;
 import pages.LoginPage;
 
 public class InvalidLogin extends BaseTest{
 
 	@Test(priority = 2,groups={"login"})
 	public void testInvalidLogin() {
+		
+		 String un=Excel.getData(XL_PATH, "InvalidLogin", 1, 0);
+		 String pw=Excel.getData(XL_PATH, "InvalidLogin", 1, 1);
+		 String failMSG=Excel.getData(XL_PATH, "InvalidLogin", 1, 2);
+		 
+		 
 //		   1. Enter Invalid User Name -abcd
 			LoginPage loginPage=new LoginPage(driver);
-			loginPage.setUserName("abcd");
+			loginPage.setUserName(un);
 //		    2. Enter Invalid Password- xyz
-			loginPage.setPassword("xyz");
+			loginPage.setPassword(pw);
 //		    3. click on login button
 			loginPage.clickLoginButton();
 //		    4. Verify that Error Msg is Displayed
 			boolean result = loginPage.verifyErrMsgDisplayed(wait);
-			softAssert.assertTrue(result);
+			softAssert.assertTrue(result, failMSG);
 			softAssert.assertAll();
 	}
 	
